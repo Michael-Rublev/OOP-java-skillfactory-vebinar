c01b455e36b9376ff49fde5fd73803cdba7fd18f
Michael <m.cmsrus@gmail.com>
2022-11-16 16:33:56 +0100

    that first OOP vebinar + version control git. glhf

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Car mazda = new Car();
+        mazda.brand = "Mazda";
+        mazda.horsePower = 190;
+        mazda.isAwd = false;
+        System.out.println(mazda.brand);
+    }
+}
